5c897c4e973534f83beaa6e28616c9a9903e937f
vicky <srivicky2000@gmail.com>
2021-04-10 07:06:29 +0530

    Contollers

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,14 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const mysql = require('mysql');
+const cors = require('cors');
 const app = express();
+const authRoutes = require('./routes/auth');
+app.use(express.json());
+app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: true }));
 var mysql = require('mysql');
 app.get("/", (req, res) => {
     res.send("Good")
